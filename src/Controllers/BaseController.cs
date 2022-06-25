1592366c31002ffea7b3ea6d7831033552f88b39
PartumGames <37095216+PartumGames@users.noreply.github.com>
2022-06-25 17:56:01 -0500

    Update BaseController.cs

--- src/Controllers/BaseController.cs
+++ src/Controllers/BaseController.cs
@@ -1,8 +1,14 @@
+using MapEditor.src.Contexts;
 
 namespace MapEditor.src.Controllers
 {
     public abstract class BaseController
     {
+        public BaseController()
+        {
+            ControllerContext.Instance.Add(this);
+        }
+
         public virtual void Load(){}
         public virtual void ForceRefresh(){}
         protected virtual void HandleUI(){}
