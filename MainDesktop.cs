30322940d6abe085659b14ad9d19e724645c9be2
PartumGames <37095216+PartumGames@users.noreply.github.com>
2022-06-25 17:16:07 -0500

    Initial commit

---
+++ MainDesktop.cs
@@ -0,0 +1,21 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+using EditorUI_DX;
+using EditorUI_DX.Controls;
+using EditorUI_DX.Utils;
+
+namespace MapEditor
+{
+    public class MainDesktop : Desktop
+    {
+        public MainDesktop(GraphicsDevice _graphics, ContentManager _content, GameWindow _window, string _defaultFontName) : base(_graphics, _content, _window, _defaultFontName)
+        {
+
+        }
+    }
+}
+
+
